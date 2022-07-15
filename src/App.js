b08e022933abe817e12ac9765e7f11ddffb7bc6c
piyush <piyushsharmashpr@gmail.com>
2022-07-15 21:36:37 +0530

    created requires and redirects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { RedirectAuth } from "./Hooks/Auth/RedirectAuth";
+import { RequiresAuth } from "./Hooks/Auth/RequiresAuth";
 import { LandingPage } from "./Pages/LandingPage";
-import {Login} from "./Pages/Login";
+import { Login } from "./Pages/Login";
 import { NotificationPage } from "./Pages/NotificationPage";
 import { Signup } from "./Pages/Signup";
 
@@ -10,10 +12,14 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<LandingPage/>}/>
-            <Route path="/notification" element={<NotificationPage/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<Signup/>}/>
+          <Route path="/" element={<LandingPage />} />
+          <Route element={<RequiresAuth />}>
+            <Route path="/notification" element={<NotificationPage />} />
+          </Route>
+          <Route element={<RedirectAuth/>}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
