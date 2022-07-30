1f6917b30085febb081699590015f8e53889eb75
piyush <piyushsharmashpr@gmail.com>
2022-07-30 23:35:27 +0530

    [feat] edit button instead of follow/unfollow in user's profile

--- src/components/FollowIcon/FollowIcon.jsx
+++ src/components/FollowIcon/FollowIcon.jsx
@@ -1,8 +1,8 @@
 import { useDispatch } from "react-redux";
 import { setFollowUsersHandler, setUnFollowUsersHandler } from "../../Hooks/slices/usersSlice";
 import Button from '@mui/material/Button';
-export const FollowIcon = ({userDetailsFromDB,user}) => {
-    const dispatch=useDispatch()
+export const FollowIcon = ({ userDetailsFromDB, user }) => {
+    const dispatch = useDispatch()
 
     function followUserTrigger(user) {
         dispatch(setFollowUsersHandler(user))
@@ -10,6 +10,8 @@ export const FollowIcon = ({userDetailsFromDB,user}) => {
     function UnfollowUserTrigger(user) {
         dispatch(setUnFollowUsersHandler(user))
     }
+    // check if it;s the user's profile
+    const checkIfUserProfile = userDetailsFromDB === user
     // to check if the user is following an account
     let isFollowing;
     return <>
@@ -19,9 +21,14 @@ export const FollowIcon = ({userDetailsFromDB,user}) => {
             })
         }
         {
-            isFollowing ? <Button variant="contained" onClick={() => UnfollowUserTrigger(user)}>Following</Button> :
-                <Button variant="outlined" onClick={() => followUserTrigger(user)}>Follow</Button>
+            checkIfUserProfile ?
+                <Button variant="contained">Edit Profile</Button>
+                :
+                isFollowing ? <Button variant="contained" onClick={() => UnfollowUserTrigger(user)}>Following</Button> :
+                    <Button variant="outlined" onClick={() => followUserTrigger(user)}>Follow</Button>
         }
+
+
 
     </>
 
